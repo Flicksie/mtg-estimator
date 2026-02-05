fb433694a4e04305eef68dbeae7e95484b094df8
Flicky@Neptune <lucasflicky@hotmail.com>
2026-02-05 01:10:01 -0300

    fix timers

--- frontend/src/composables/alerts/useExportArchidektAlerts.ts
+++ frontend/src/composables/alerts/useExportArchidektAlerts.ts
@@ -8,7 +8,7 @@ export const useExportArchidektAlerts = () => {
       title: 'Exported to Archidekt!',
       html: 'Your collection has been exported as TXT.<br><br>You can import this file into Archidekt by:<br>1. Go to <a href="https://www.archidekt.com" target="_blank" rel="noopener noreferrer">archidekt.com</a><br>2. Create or edit a deck<br>3. Use the import/paste function to add the cards',
       icon: 'success',
-      timer: 5000,
+      timer: 35000,
       timerProgressBar: true
     })
   }
